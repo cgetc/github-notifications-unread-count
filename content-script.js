1ab08ac7aeb631497828f936305b05bc33aaf74c
cgetc <cgetc502@gmail.com>
2024-12-04 21:47:33 +0900

    bootstrap

---
+++ content-script.js
@@ -0,0 +1,22 @@
+function main() {
+    if (document.querySelector('section[aria-labelledby="single-sign-on"]')) {
+        document.title = `❗${document.title}`
+        return
+    }
+
+    const unreadCount = document.querySelectorAll('.notification-unread').length
+    if (unreadCount) {
+        const text = unreadCount >= 25 ? '25+' : new String(unreadCount)
+        document.title = `(${text})${document.title}`
+    }
+
+    setTimeout(reload, 60000)
+}
+
+function reload() {
+    if (document.hidden) {
+        location.reload()
+    }
+}
+
+main()
